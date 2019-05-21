4e10bea5fe8d4beecac3a90fb3ec2d54b9392c7d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-05-22 00:11:12 +0200

    Integrate store with router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import Main from './components/Main';
-import PhotoGrid from './components/PhotoGrid';
-import Single from './components/Single';
-import './styles/style.styl';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
+import Main from "./components/Main";
+import PhotoGrid from "./components/PhotoGrid";
+import Single from "./components/Single";
+import "./styles/style.styl";
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid} />
-      <Route path="/view/:postId" component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
-ReactDOM.render(router, document.getElementById('root'));
+ReactDOM.render(router, document.getElementById("root"));
