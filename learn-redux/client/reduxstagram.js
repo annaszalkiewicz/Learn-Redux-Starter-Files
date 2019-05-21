8e011a4750fce6e4de9ce9c152a08685b4dbdc62
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-05-21 21:04:01 +0200

    Render ReactDOM

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,6 @@
-// let's go!
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+import './styles/style.styl';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
