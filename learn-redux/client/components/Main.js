31f10dae6b4ed19348cf633687293ea4f60c96c6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-05-21 21:38:11 +0200

    Create PhotoGrid component

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, { Component } from "react";
+import { Link } from "react-router";
+import PhotoGrid from "./PhotoGrid";
 
 class Main extends Component {
   render() {
     return (
-      <h1>
-        <Link to="/">Reduxstagram</Link>
-      </h1>
+      <React.Fragment>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+        <PhotoGrid />
+      </React.Fragment>
     );
   }
 }
---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PhotoGrid extends Component {
+  render() {
+    return (
+      <div className="photo-grid">PhotoGrid component!</div>
+    );
+  }
+}
+
+export default PhotoGrid;
